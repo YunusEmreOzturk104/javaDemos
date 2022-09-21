73760b1274ffc167b331844f3de38c9b84dd39f3
YunusEmreOzturk104 <ozturkyunusemre511@gmail.com>
2022-09-21 18:51:05 +0300

    .

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,18 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi=20;
+		if (sayi<20) {
+			System.out.println("Sayý 20den küçüktür");
+		}
+		else if (sayi==20){
+			System.out.println("Sayý 20dir");
+		}
+		else  {
+			System.out.println("Sayý 20den buyuk");
+		}
+	}
+
+}
